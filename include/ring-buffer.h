2dfe15629d7fc30bdba2f61e66c08e49c1ceac44
sdp-io <samuelpetersonacs@gmail.com>
2026-07-28 20:07:09 -0400

    Finalize ring buffer implementation and add basic tests to main

--- include/ring-buffer.h
+++ include/ring-buffer.h
@@ -2,19 +2,35 @@
 
 #include <concepts>
 #include <cstddef>
+#include <stdexcept>
+
+// NOTE: Rule of 5 and Rule of 0. Remember what they are and WHY
 
 template <std::movable T> class RingBuffer
 {
 public:
   RingBuffer(const size_t bs) : capacity{bs}, ring_buffer{new T[capacity]} {}
 
+  RingBuffer(const RingBuffer& rb) : capacity{rb.capacity}
+  { deep_copy(rb); }
+
+  RingBuffer(const RingBuffer&& rb) : capacity{rb.capacity}
+  { deep_copy(rb); }
+
+  RingBuffer<T>& operator=(const RingBuffer& rb);
+
   ~RingBuffer()
   { delete[] ring_buffer; }
 
-  void enqueue(const T& item);
-
   // NOTE: Remember move semantics, l-values, r-values!
-  void enqueue(const T&& item);
+  void enqueue(const T& item)
+  { add_item(item); }
+
+  void enqueue(const T&& item)
+  { add_item(item); }
+
+  bool is_empty()
+  { return count == 0; }
 
   T dequeue();
 
@@ -25,21 +41,12 @@ private:
   size_t write{0};
   size_t count{0};
 
-  void deep_copy(const RingBuffer& source)
-  {
-    delete[] ring_buffer;
+  void deep_copy(const RingBuffer& source);
 
-    capacity = source.capacity;
-
-    ring_buffer = new T[capacity];
-
-    while (source.count > 0) {
-      this->enqueue(source.dequeue());
-    }
-  }
+  void add_item(const T& item);
 };
 
-template <std::movable T> void RingBuffer<T>::enqueue(const T& item)
+template <std::movable T> void RingBuffer<T>::add_item(const T& item)
 {
   ring_buffer[write] = item;
   write = (write + 1) % capacity;
@@ -51,6 +58,12 @@ template <std::movable T> void RingBuffer<T>::enqueue(const T& item)
 
 template <std::movable T> T RingBuffer<T>::dequeue()
 {
+  // NOTE: Alternatively could have used an assert() for speed, or C++17 std::optional return value.
+  // All approaches are valid with their own tradeoffs.
+  if (is_empty()) {
+    throw std::logic_error("RingBuffer is empty! Did you try calling is_empty() first?\n");
+  }
+
   T item{ring_buffer[read]};
   read = (read + 1) % capacity;
 
@@ -59,4 +72,25 @@ template <std::movable T> T RingBuffer<T>::dequeue()
   }
 
   return item;
+}
+
+template <std::movable T> void RingBuffer<T>::deep_copy(const RingBuffer& source)
+{
+  delete[] ring_buffer;
+
+  ring_buffer = new T[capacity];
+
+  for (size_t i{0}; i < source.count; ++i) {
+    ring_buffer[i] = source.ring_buffer[i];
+    count++;
+  }
+}
+
+template <std::movable T> RingBuffer<T>& RingBuffer<T>::operator=(const RingBuffer<T>& rb)
+{
+  capacity = rb.capacity;
+
+  deep_copy(rb);
+
+  return *this;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,49 @@
+#include "ring-buffer.h"
 #include <iostream>
 
 int main()
-{ std::cout << "Testing 123\n"; }
+{
+  RingBuffer<int> rb1{10};
+
+  rb1.enqueue(1);
+  rb1.enqueue(2);
+  rb1.enqueue(3);
+  rb1.enqueue(4);
+  rb1.enqueue(5);
+
+  int v1{rb1.dequeue()};
+  int v2{rb1.dequeue()};
+  int v3{rb1.dequeue()};
+
+  std::cout << "v1: " << v1 << "\n";
+  std::cout << "v2: " << v2 << "\n";
+  std::cout << "v3: " << v3 << "\n";
+
+  std::cout << "Testing copy constructor. . .\n";
+
+  RingBuffer<int> rb2{rb1};
+
+  std::cout << "rb2 is_empty(): " << rb2.is_empty() << std::endl;
+  rb2.dequeue();
+  rb2.dequeue();
+
+  std::cout << "Testing overloaded assignment operator. . .\n";
+
+  rb1 = rb2;
+
+  std::cout << "Testing move constructor. . .\n";
+
+  RingBuffer<int> rb3{std::move(rb1)};
+
+  for (int i{0}; i < 100; ++i) {
+    // NOTE: Commented out for testing std::logic_error throw on empty buf removal
+    // rb3.dequeue();
+    rb2.enqueue(i);
+  }
+
+  for (int i{0}; i < 10; ++i) {
+    // NOTE: Commented out for testing std::logic_error throw on empty buf removal
+    // std::cout << "rb1 item: " << rb1.dequeue() << "\n";
+    std::cout << "rb2 item: " << rb2.dequeue() << "\n";
+  }
+}
